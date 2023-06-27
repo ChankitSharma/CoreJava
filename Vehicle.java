50501895d720e18193badc041ad8355638e8fb63
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 19:35:10 +0530

    Practical 13 v3 Abstraction

--- Vehicle.java
+++ Vehicle.java
@@ -1,9 +1,15 @@
+/**
+ * Vehicle is Abstract class
+ */
 public abstract class Vehicle {
 
-    int tyre ;
+	int tyre;
+
+	/* This is the Non abstract method */
 	void showTyre(int value) {
 		tyre = value;
 	}
+
+	/* This is Abstract method */
 	abstract void start();
 }
- 
