58c9e2d7a579a4fa067a70289b293ceedd043d2b
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 19:23:51 +0530

    Practical 15 Single inheritance v3

--- Animals.java
+++ Animals.java
@@ -1,11 +1,13 @@
 /**
- * Base class
+ * Animal class is the Base class of single inheritance
  */
 public class Animals {
 
+	/*This is the eat method*/
 	void eat() {
 		System.out.println("Eating...");
 	}
+	/*This is the run method*/
 	void run() {
 		System.out.println("Running...");
 	}
--- Dogs.java
+++ Dogs.java
@@ -3,13 +3,18 @@
  */
 public class Dogs extends Animals {
 
+	/*This is the the method for barking*/
 	void bark() {
 		System.out.println("Barking...");
 	}
+	/*Main method of that class*/
 	public static void main(String[] args) {
 		Dogs dog = new Dogs();
+		/*Calling eat method from Animal class*/
 		dog.eat();
+		/*Calling bark method from Dogs class*/
 		dog.bark();
+		/*Calling eat method from Animal class*/
 		dog.run();
 	}
 }
