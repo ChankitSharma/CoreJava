1f4d288ac167e8785119ed9035d42db38039b4a1
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 14:50:22 +0530

    Practical 20 v2

---
+++ AddString.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+/**
+ * Adding String in List using iterator
+ * */
+public class AddString {
+
+	public static void main(String[] args) {
+		
+		List<String> name = new ArrayList<>();
+		
+		name.add("Hello");
+		name.add("world");
+		name.add("$chankit");
+		
+		Iterator<String> iterator = name.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
+	
+}
