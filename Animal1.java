1f9509102fcdeb0c768d9e0f46e029a62595213a
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 19:51:54 +0530

    Practical 17 v3

--- Animal1.java
+++ Animal1.java
@@ -1,8 +1,10 @@
 /**
- * Base class for performing Abstraction
+ * Interface class for performing Abstraction
  */
 public interface Animal1 {
 
+	/*eat is the Abstract method*/
 	void eat();
+	/*sleep is the Abstract method*/
 	void sleep();
 }
--- Main1.java
+++ Main1.java
@@ -3,8 +3,11 @@
  */
 public class Main1 {
 
+	/*This is the main method
+	 * In this we are performing Abstraction*/
 	public static void main(String[] args) {
 		Cat1 cat = new Cat1();
+		/*Calling Abstract methods*/
 		cat.eat();
 		cat.sleep();
 	}
