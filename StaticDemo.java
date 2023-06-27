a0648fe05eb9ed761bfd3025c2f18842397a4ce0
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 12:59:05 +0530

    Practical 12 v2

---
+++ StaticDemo.java
@@ -0,0 +1,30 @@
+/**
+This is Static Variable. 
+*/
+public class StaticDemo {
+
+	static int value ;
+	
+	/*
+	  This is Static Method.  
+	*/
+	static void show()
+	{
+		System.out.println("This is Static Method");
+	}
+	
+	/*
+	  This is Static Block .
+	*/
+	static 
+	{
+		System.out.println("This is static block");
+		value = 10;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(value + " is static variable");
+		StaticDemo.show();
+	}
+	
+}
