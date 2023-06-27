cab281668acebc8f8cfd42df440644964b20d464
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 13:04:36 +0530

    Practical 14 v2

---
+++ Encapsulation.java
@@ -0,0 +1,23 @@
+/**
+ * The Encapsulation class demonstrates encapsulation by encapsulating
+ * employee information such as ID and name.
+ */
+public class Encapsulation {
+
+	private int employee_id;
+	private String name;
+	
+	public void setEmployeeId(int x) {
+		employee_id = x;
+	}
+	public int getEmployeeId() {
+		return employee_id;
+	}
+	public void setEmployeeName(String y) {
+		name=y;
+	}
+	public String getEmployeeName() {
+		return name;
+	}
+	
+}
