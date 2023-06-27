a74708a6a73a31241d7443bc1b4d021af79fd22b
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 13:38:42 +0530

    Practical 16 v2

---
+++ OverRiding.java
@@ -0,0 +1,11 @@
+/**
+In this class we have method implementation for achiving Polymorphism
+ */
+public class OverRiding {
+
+	void show () {
+		String name = "Mike";
+		String City = "Banglore";
+	}
+	
+}
