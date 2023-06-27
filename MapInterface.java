cb217b9644943b61c87d6f1e2c03813bfc7ad6d5
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 14:59:00 +0530

    Practical 21 v2

---
+++ MapInterface.java
@@ -0,0 +1,21 @@
+package Practical_21;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*Implementation of Map Interface*/
+public class MapInterface {
+
+	public static void main(String[] args) {
+		
+		Map<Integer,String> map = new HashMap<>();
+		
+		map.put(10, "Mike");
+		map.put(20,"jhone");
+		map.replace(20,"Tyson");
+		
+		System.out.println(map);
+		System.out.println(map.entrySet());
+	}
+	
+}
---
+++ SetInterface.java
@@ -0,0 +1,19 @@
+/**
+ * Implementation of Set Interface
+ */
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetInterface {
+
+	public static void main(String[] args) {
+		
+		Set<Integer> set = new HashSet<>();
+		set.add(100);
+		set.add(200);
+		set.add(300);
+		
+		System.out.println("Does the Set contains 300 :" + set.contains(300));
+		System.out.println("Set interface added elements :" + set);
+	}
+}
