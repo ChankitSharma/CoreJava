96bb75c88c1e82164da4be484009849fd483ba1a
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 13:47:29 +0530

    Practical 18 v2

---
+++ Voting.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/**
+ * This class is created for validation for voting
+ */
+public class Voting {
+
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your age : ");
+		
+	try {
+		int age = scan.nextInt();
+		
+		if(age < 18) {
+			throw new YongerAgeException("Invalide age");
+		} 
+		else
+		{
+			System.out.println("Eligible for vote");
+		}
+		}catch(YongerAgeException e) {
+			System.out.println("Exception Occour");
+		}
+	finally {
+		scan.close();
+		}
+	}
+}
---
+++ YongerAgeException.java
@@ -0,0 +1,9 @@
+/**
+ * This is created for custom made exception for voting class
+ */
+public class YongerAgeException extends RuntimeException{
+
+	YongerAgeException(String msg) {
+		super(msg);
+	}
+}
