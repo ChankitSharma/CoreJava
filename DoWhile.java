6903177d73346df4d471ee5f8c67e79590d3e291
ChankitSharma <chankitsharma.cs@gmail.com>
2023-06-27 12:48:35 +0530

    Practical 4 v2

---
+++ DoWhile.java
@@ -0,0 +1,18 @@
+/**
+ * This is DoWhile class  
+ * */
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		int number = 1;
+		
+		do 
+		{
+			System.out.println(number);
+			++number;
+		}
+		while(number <= 10);
+	}
+	
+}
---
+++ WhileLoop.java
@@ -0,0 +1,16 @@
+public class WhileLoop {
+/**
+  In this class we are using while loop 
+ */
+	public static void main(String[] args) {
+		
+		int number = 1;
+		
+		while(number <= 10)
+		{
+			System.out.println(number);
+			++number;
+		}
+	}
+	
+}
