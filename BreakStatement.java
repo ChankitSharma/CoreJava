94881271f5597e3af9eee28cdc10b38aca7065ed
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 20:26:58 +0530

    Practical 6 v3 - Applied code formation

--- BreakStatement.java
+++ BreakStatement.java
@@ -1,20 +1,21 @@
 /**
- *This program demonstrates the usage of 'continue' and 'break' statements in a loop.
+ * This program demonstrates the usage of 'continue' and 'break' statements in a
+ * loop.
+ *
+ * @author Chankit
  */
 public class BreakStatement {
 
 	public static void main(String[] args) {
-
-		for(int i=1; i<=10; i++)
-		{
-			if(i == 5) {
+		for (int i = 1; i <= 10; i++) {
+			if (i == 5) {
 				continue;
 			}
 			System.out.println(i);
-			if(i == 7) {
+			if (i == 7) {
 				break;
 			}
 		}
 	}
-	
+
 }
