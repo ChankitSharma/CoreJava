83d926eb839f50879794e0a5cf418429c24c7367
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 17:32:10 +0530

    Practical 2 v3 - Applied code formation

--- AgeValidation.java
+++ AgeValidation.java
@@ -1,17 +1,17 @@
 /**
  * Validating age group by using if else statement
+ * 
+ * @author Chankit
  */
 public class AgeValidation {
 
 	public static void main(String[] args) {
-		
 		int age = 17;
-		
 		if (age < 18) {
 			System.out.println("You are under 18");
-		}
-		else {
+		} else {
 			System.out.println("You are adult");
 		}
 	}
+
 }
