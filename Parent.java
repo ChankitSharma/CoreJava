de2fbe8f33eeca35c51cabd1130431e7527ce30d
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 21:23:43 +0530

    Practical 11 v3 - Applied code formation

--- Parent.java
+++ Parent.java
@@ -1,3 +1,8 @@
+/**
+ * This is the Parent class for purpose of performing this and super keyword Examples
+ * 
+ * @author Chankit
+ */
 public class Parent 
 {
 	int value =10;
--- SuperKeyword.java
+++ SuperKeyword.java
@@ -1,19 +1,21 @@
 /**
- *Extending parent class and calling both value variables using this and super keyword. 
+ * Extending parent class and calling variable using this and super keyword.
+ *
+ * @author Chankit
  */
 public class SuperKeyword extends Parent {
-	
+
 	int value = 20;
-	void show(int value) 
-	{
+
+	void show(int value) {
 		System.out.println(this.value);
 		System.out.println(super.value);
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		SuperKeyword superKey = new SuperKeyword();
 		superKey.show(30);
 	}
-	
+
 }
