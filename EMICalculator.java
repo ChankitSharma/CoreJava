955ae47ddd385a8de305e483ee482293ddcd1982
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 20:35:48 +0530

    Practical 8 v3 - Applied code formation - I have used chat-gpt for a method documentation.

--- EMICalculator.java
+++ EMICalculator.java
@@ -2,31 +2,42 @@ import java.util.Scanner;
 
 /**
  * This Class is used for EMI calculation
+ * 
+ * @author Chankit
  */
 public class EMICalculator {
 
-	 public static void main(String[] args) {
- 
-	        Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
-	        System.out.print("Enter the principal amount: ");
-	        double principal = scanner.nextDouble();
+		System.out.print("Enter the principal amount: ");
+		double principal = scanner.nextDouble();
 
-	        System.out.print("Enter the annual interest rate (%): ");
-	        double interestRate = scanner.nextDouble() / 100.0;
-	        double monthlyRate = interestRate / 12.0;
+		System.out.print("Enter the annual interest rate (%): ");
+		double interestRate = scanner.nextDouble() / 100.0;
+		double monthlyRate = interestRate / 12.0;
 
-	        System.out.print("Enter the loan tenure (in months): ");
-	        int years = scanner.nextInt();
+		System.out.print("Enter the loan tenure (in months): ");
+		int years = scanner.nextInt();
 
-	        double emi = calculateEMI(principal, monthlyRate, years);
-	        System.out.println("The Equated Monthly Installment (EMI) is: " + emi);
+		double emi = calculateEMI(principal, monthlyRate, years);
+		System.out.println("The Equated Monthly Installment (EMI) is: " + emi);
 
-	        scanner.close();
-	    }
+		scanner.close();
+	}
 
-	    public static double calculateEMI(double principal, double monthlyRate, int years) {
-	    	 return (principal * monthlyRate * Math.pow(1 + monthlyRate, years)) / (Math.pow(1 + monthlyRate, years) - 1);
-	    }
+	/**
+	 * Calculates the Equated Monthly Installment (EMI) for a loan based on the
+	 * given parameters.
+	 *
+	 * @param principal   The principal amount of the loan.
+	 * @param monthlyRate The monthly interest rate, calculated from the annual
+	 *                    interest rate.
+	 * @param years       The loan tenure in months.
+	 * @return The calculated Equated Monthly Installment (EMI) for the loan.
+	 */
+	public static double calculateEMI(double principal, double monthlyRate, int years) {
+		return (principal * monthlyRate * Math.pow(1 + monthlyRate, years)) / (Math.pow(1 + monthlyRate, years) - 1);
+	}
 
 }
