6cc11a8e48058e9516c03e5f490142a456e9e165
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 21:43:37 +0530

    Practical 14 v3 - Applied code formation

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,23 +1,28 @@
 /**
- * The Encapsulation class demonstrates encapsulation by encapsulating
- * employee information such as ID and name.
+ * The Encapsulation class demonstrates encapsulation by encapsulating employee
+ * information such as ID and name.
+ * 
+ * @author Chankit
  */
 public class Encapsulation {
 
 	private int employee_id;
 	private String name;
-	
+
 	public void setEmployeeId(int x) {
 		employee_id = x;
 	}
+
 	public int getEmployeeId() {
 		return employee_id;
 	}
+
 	public void setEmployeeName(String y) {
-		name=y;
+		name = y;
 	}
+
 	public String getEmployeeName() {
 		return name;
 	}
-	
+
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
 /**
- * This is the Main class for the Encapsulation
- * */
+ * This is the Main class of the Encapsulation
+ * 
+ * @author Chankit
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Encapsulation employee = new Encapsulation();
-		
+
 		employee.setEmployeeId(10);
 		employee.setEmployeeName("Chankit");
 		System.out.println(employee.getEmployeeId());
 		System.out.println(employee.getEmployeeName());
 	}
-	
+
 }
