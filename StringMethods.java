b1bc84886147f34642214210ed028d5bec487a38
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 20:44:20 +0530

    Practical 10 v3 - Applied code formation

--- StringMethods.java
+++ StringMethods.java
@@ -1,16 +1,17 @@
 /**
- *This is the String Methods class for presenting String Methods.
+ * This is the String Methods class for presenting String Methods.
+ *
+ * @author Chankit
  */
 public class StringMethods {
 
 	public static void main(String[] args) {
-		
 		String name1 = "Chankit";
 		String name2 = "Sharma";
-		
+
 		System.out.println("Compare two Strings: " + name1.equals(name2));
 		System.out.println("Find Charctor in String: " + name1.charAt(5));
 		System.out.println("Concating two String: " + name1.concat(name2));
 	}
-	
+
 }
