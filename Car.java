77a22ff5dcbaff6d5e7f152e8ff244ab77cf463f
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 21:36:40 +0530

    Practical 13 v4 - Applied code formation

--- Car.java
+++ Car.java
@@ -1,25 +1,19 @@
 /**
  * This class is extending Vehicle class for achiving Abstraction.
+ * 
+ * @author Chankit
  */
 public class Car extends Vehicle {
 
 	void start() {
-		/**
-		 * This is the implementation of Abstract method 
-		 */
+		// This is the implementation of Abstract method
 		System.out.println("Car Start with key");
 		System.out.println("Number of tyre's Car have" + tyre);
 	}
 
-	/**
-	 *  Main method of that class 
-	 */
 	public static void main(String[] args) {
 
 		Vehicle vehicle = new Car();
-		/**
-		 * Calling perent class method
-		 */
 		vehicle.showTyre(4);
 		vehicle.start();
 	}
--- Vehicle.java
+++ Vehicle.java
@@ -1,18 +1,22 @@
 /**
- * Vehicle is Abstract class
+ * This program is demonstrate to Abstraction
+ * 
+ * @author Chankit
  */
 public abstract class Vehicle {
 
 	int tyre;
-	/* 
-	 * This is the Non abstract method 
-	 * */
+
+	/**
+	 * This is the Non abstract method
+	 */
 	void showTyre(int value) {
 		tyre = value;
 	}
 
-	/* 
-	 * This is Abstract method 
-	 * */
+	/**
+	 * This is Abstract method
+	 */
 	abstract void start();
+
 }
