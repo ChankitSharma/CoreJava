75e543c37e1a9726a0ad198b3c0eb0e0042e6aca
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 20:24:29 +0530

    Practical 5 v3 - Applied code formation

--- StarPattern.java
+++ StarPattern.java
@@ -1,27 +1,20 @@
 /**
- * This class is performing star patterns 
- * */
+ * This class is performing star patterns
+ * 
+ * @author Chankit
+ */
 public class StarPattern {
 
 	public static void main(String[] args) {
-		
-		/*
-		 Outer loop to control the number of rows 
-		*/
-		for(int i=1; i<=5; i++)
-		{
-			/*
-			 Inner loop to print the stars in each row 
-			*/
-			for(int j=1; j<=i; j++)
-			{
-				System.out.print(" *");	
+		// Outer loop to control the number of rows
+		for (int i = 1; i <= 5; i++) {
+			// Inner loop to print the stars in each row
+			for (int j = 1; j <= i; j++) {
+				System.out.print(" *");
 			}
-			/*
-			 Move to the next line after printing stars in a row 
-			*/
+			// Move to the next line after printing stars in a row
 			System.out.println();
 		}
 	}
-	
+
 }
