dd89afc09c4f00f97b77c9e58f0fd6187188cea0
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 21:25:25 +0530

    Practical 12 v3 - Applied code formation

--- StaticDemo.java
+++ StaticDemo.java
@@ -1,30 +1,27 @@
 /**
- *This is Static Variable. 
+ * This program to demonstrate static methods, variables, and blocks.
+ *
+ * @author Chankit
  */
 public class StaticDemo {
 
-	static int value ;
-	
-	/*
-	  This is Static Method.  
-	*/
-	static void show()
-	{
+	//This is Static Variables
+	static int value;
+
+	//This is Static Method.
+	static void show() {
 		System.out.println("This is Static Method");
 	}
-	
-	/*
-	  This is Static Block .
-	*/
-	static 
-	{
+
+	// This is Static Block .
+	static {
 		System.out.println("This is static block");
 		value = 10;
 	}
-	
+
 	public static void main(String[] args) {
 		System.out.println(value + " is static variable");
 		StaticDemo.show();
 	}
-	
+
 }
