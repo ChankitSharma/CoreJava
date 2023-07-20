373de8a3afd2757a7aae423b0d212518291e89d4
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-20 17:38:57 +0530

    Practical 4 v3 - Applied code formation

--- DoWhile.java
+++ DoWhile.java
@@ -1,18 +1,16 @@
 /**
- * This is DoWhile class  
- * */
+ * This is DoWhile class
+ * 
+ * @author Chankit
+ */
 public class DoWhile {
 
 	public static void main(String[] args) {
-		
 		int number = 1;
-		
-		do 
-		{
+		do {
 			System.out.println(number);
 			++number;
-		}
-		while(number <= 10);
+		} while (number <= 10);
 	}
-	
+
 }
--- WhileLoop.java
+++ WhileLoop.java
@@ -1,16 +1,16 @@
-public class WhileLoop {
 /**
- * In this class we are using while loop 
+ * In this class we are using while loop
+ * 
+ * @author Chankit
  */
+public class WhileLoop {
+
 	public static void main(String[] args) {
-		
 		int number = 1;
-		
-		while(number <= 10)
-		{
+		while (number <= 10) {
 			System.out.println(number);
 			++number;
 		}
 	}
-	
+
 }
