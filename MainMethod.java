591206392699b14174152d3a4d8a756e7cd3a22a
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-21 13:28:50 +0530

    Practical 18 Throws v3 - Applied code formation

--- MainMethod.java
+++ MainMethod.java
@@ -1,5 +1,7 @@
 /**
- * This is the main method for the accessing main files in our system. 
+ *This program is demonstrate Throws keyword
+ *
+ * @author Chankit
  */
 import java.io.FileNotFoundException;
 
@@ -14,4 +16,5 @@ public class MainMethod {
 		}
 		System.out.println("Rest of the code...");
 	}
+
 }
--- ReadAndWrite.java
+++ ReadAndWrite.java
@@ -1,14 +1,20 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-/*
+
+/**
  * Creating a read and write class for access files in system
- * */
+ * 
+ * @author Chankit
+ */
 public class ReadAndWrite {
 
-	/*
-	 * creating read file method for access the main file and used throws keyword for the checked exception
-	 * */
+	/**
+	 * This is the readFile method for access the main file and used throws keyword for the checked exception
+	 * 
+	 * @exception FileNotFoundException
+	 */
 	public static void readFile() throws FileNotFoundException {
 		FileInputStream file = new FileInputStream("E:\\tomacate.txt");
 	}
+
 }
