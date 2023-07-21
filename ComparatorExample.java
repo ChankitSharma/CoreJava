84bbaf29d8750b008f3b04b53e8adc0072ce4936
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-21 23:39:26 +0530

    Practical 23 v2 Comparator - Applied code formation

---
+++ ComparatorExample.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ComparatorExample{
+
+	public static void main(String[] args) {
+		List<Person> personList = new ArrayList<>();
+		personList.add(new Person("Bob", 25));
+		personList.add(new Person("Alice", 25));
+		personList.add(new Person("Charlie", 20));
+		personList.add(new Person("Alice", 20));
+		personList.add(new Person("Bob", 30));
+		personList.add(new Person("Charlie", 20));
+
+		// This method is sort name using comparator
+		Collections.sort(personList, new NameComparator());
+		for (Person person : personList) {
+			System.out.println(person.getName() + ": " + person.getAge());
+		}
+	}
+	
+}
