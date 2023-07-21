44a32f12a503838019b0a6e7662c877dc7bdbb65
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-21 20:21:10 +0530

    Practical 23 v2 Comparator - Applied code formation

--- Person.java
+++ Person.java
@@ -1,10 +1,12 @@
+package demo;
+
 /**
  * Program to demonstrate comparable and comparator interface, This Class is
  * used to implements comparable
  * 
  * @author Chankit
  */
-public class Person {
+public class Person  {
 
 	private String name;
 	private int age;
