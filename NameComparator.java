5f4918f85ec27e0dea0c990da35cfa791256a0cf
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-21 20:18:30 +0530

    Practical 23 v2 Comparable - Applied code formation

---
+++ NameComparator.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+
+/**
+ * This class is performing Comparator
+ */
+public class NameComparator implements Comparator<Person> {
+
+	@Override
+	public int compare(Person o1, Person o2) {
+		int nameComparison = o1.getName().compareTo(o2.getName());
+		if (nameComparison == 0) {
+			return Integer.compare(o1.getAge(), o2.getAge());
+		}
+		return nameComparison;
+	}
+
+}
--- Person.java
+++ Person.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 /**
- * In thid program we are performing Age comparison
- * */
-class Person {
+ * Program to demonstrate comparable and comparator interface, This Class is
+ * used to implements comparable
+ * 
+ * @author Chankit
+ */
+public class Person {
 
 	private String name;
 	private int age;
@@ -22,32 +21,5 @@ class Person {
 	public int getAge() {
 		return age;
 	}
-}
 
-class AgeComparator implements Comparator<Person> {
-	@Override
-	public int compare(Person p1, Person p2) {
-		return p1.getAge() - p2.getAge();
-	}
-}
-
-class ComparatorExample {
-	public static void main(String[] args) {
-		List<Person> personList = new ArrayList<>();
-		personList.add(new Person("Alice", 25));
-		personList.add(new Person("Bob", 30));
-		personList.add(new Person("Charlie", 20));
-
-		System.out.println("Before sorting:");
-		for (Person person : personList) {
-			System.out.println(person.getName() + " - " + person.getAge());
-		}
-
-		Collections.sort(personList, new AgeComparator());
-
-		System.out.println("\nAfter sorting by age:");
-		for (Person person : personList) {
-			System.out.println(person.getName() + " - " + person.getAge());
-		}
-	}
 }
