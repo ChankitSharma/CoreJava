6320f957d30e285a3a4f5ecb30e64d2a23fdfd84
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-21 15:25:30 +0530

    Practical 22 v3 - Applied code formation

--- ListInterface.java
+++ ListInterface.java
@@ -1,13 +1,14 @@
 /**
- * Implementation of ListInterface
- */
+* Implementation of ListInterface
+* 
+* @author Chankit
+*/
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListInterface {
 
 	public static void main(String[] args) {
-		
 		List<String> list = new ArrayList<>();
 		list.add("A");
 		list.add("B");
@@ -18,15 +19,16 @@ public class ListInterface {
 		list.add("E");
 		list.add("E");
 		list.add("E");
-		
-		/*Creating another List object for storing unique element*/
+
+		// Creating another List object for storing unique element
 		List<String> uniqueList = new ArrayList<>();
-		
-		for(String elements : list) {
-			if(!uniqueList.contains(elements)) {
+
+		for (String elements : list) {
+			if (!uniqueList.contains(elements)) {
 				uniqueList.add(elements);
 			}
 		}
 		System.out.println(uniqueList);
 	}
+
 }
