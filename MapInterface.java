df2848b6604c00ab514e67b38f814803f45b14fb
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-21 14:53:33 +0530

    Practical 21 v4 - Applied code formation

--- MapInterface.java
+++ MapInterface.java
@@ -1,20 +1,21 @@
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  * Implementation of Map Interface
- * */
+ * 
+ * @author Chankit
+ */
 public class MapInterface {
 
 	public static void main(String[] args) {
-		
-		Map<Integer,String> map = new HashMap<>();
-		
+		Map<Integer, String> map = new HashMap<>();
 		map.put(10, "Mike");
-		map.put(20,"jhone");
-		map.replace(20,"Tyson");
-		
+		map.put(20, "jhone");
+		map.replace(20, "Tyson");
+
 		System.out.println(map);
 		System.out.println(map.entrySet());
 	}
-	
+
 }
--- SetInterface.java
+++ SetInterface.java
@@ -1,19 +1,20 @@
-/**
- * Implementation of Set Interface
- */
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ *  Implementation of Set Interface
+ *  
+ *  @author Chankit
+ */
 public class SetInterface {
-
 	public static void main(String[] args) {
-		
 		Set<Integer> set = new HashSet<>();
 		set.add(100);
 		set.add(200);
 		set.add(300);
-		
+
 		System.out.println("Does the Set contains 300 :" + set.contains(300));
 		System.out.println("Set interface added elements :" + set);
 	}
+
 }
