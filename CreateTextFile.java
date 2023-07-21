589beee928c540d53b08faebe622a194ff71b517
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-21 20:28:57 +0530

    Practical 24 v2 - Applied code formation

--- CreateTextFile.java
+++ CreateTextFile.java
@@ -4,31 +4,28 @@ import java.io.IOException;
 
 /**
  * The CreateTextFile class demonstrates how to create a text file and add personal details to it.
+ * 
+ * @author Chankit
  */
 public class CreateTextFile {
 
 	public static void main(String[] args) {
-		/**
-		 * Creating a file object and configure path and file name which we want to create.
-		 */
+		// Creating a file object and configure path and file name which we want to create.
 		File file = new File("E:\\myTextFile.txt");
-		
 		try {
-			/**
-			 * It will give  IOException for that reson we are puting code inside try catch block
-			 */
-			if(file.createNewFile()) {
+			// It will give IOException for that reson we are puting code inside try catch block
+			if (file.createNewFile()) {
 				FileWriter fileWriter = new FileWriter(file);
-				fileWriter.write("Name: Mike Tyson" +"\n" +" Age: 30"+"\n"+"Post: Software Engineer");
+				fileWriter.write("Name: Mike Tyson" + "\n" + " Age: 30" + "\n" + "Post: Software Engineer");
 				fileWriter.close();
 				System.out.println("File created successfully.");
+			} else {
+				System.out.println("File is Already Exist...");
 			}
-			else {
-				 System.out.println("File is Already Exist...");
-			}
-		} catch (IOException e) {
-			System.out.println("An error occurred while creating the file: " + e.getMessage());
+		} catch (IOException ioException) {
+			System.out.println("An error occurred while creating the file: ");
+			ioException.printStackTrace();
 		}
 	}
-	
+
 }
