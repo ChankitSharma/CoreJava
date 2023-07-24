8a28be7f061ba704fea4a436b96dc24f63ff5ebb
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-24 11:14:23 +0530

    Practical 23 v4 Comparable - Applied code formation

--- Employee.java
+++ Employee.java
@@ -1,17 +1,31 @@
+/**
+ * Program to demonstrate comparable interface.
+ * 
+ * @author Chankit
+ */
 public class Employee implements Comparable<Employee> {
 
 	private String name;
 	private int age;
 
+	/**
+	 * This is the parameterize constructor for set name and age
+	 */
 	public Employee(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
 
+	/**
+	 * This method is used for getting the Name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * This method is used for gatting the age
+	 */
 	public int getAge() {
 		return age;
 	}
@@ -19,13 +33,13 @@ public class Employee implements Comparable<Employee> {
 	@Override
 	public int compareTo(Employee other) {
 		if (this.name == null && other.name == null) {
-			return 0; 
+			return 0;
 		} else if (this.name == null) {
-			return 1; 
+			return 1;
 		} else if (other.name == null) {
 			return -1;
 		} else {
-			
+
 			int nameComparison = this.name.compareTo(other.name);
 			if (nameComparison == 0) {
 				return Integer.compare(this.age, other.age);
@@ -33,5 +47,5 @@ public class Employee implements Comparable<Employee> {
 			return nameComparison;
 		}
 	}
-	
+
 }
