d641030424f34e8cf712af201255bcb428d8e280
ChankitSharma <chankitsharma.cs@gmail.com>
2023-07-24 15:26:58 +0530

    Practical 11 v4 - Applied code formation

--- Parent.java
+++ Parent.java
@@ -1,9 +1,11 @@
 /**
- * This is the Parent class for purpose of performing this and super keyword Examples
+ * This is the Parent class for purpose of performing this and super keyword
+ * Examples
  * 
  * @author Chankit
  */
-public class Parent 
-{
-	int value =10;
+public class Parent {
+
+	int value = 10;
+
 }
